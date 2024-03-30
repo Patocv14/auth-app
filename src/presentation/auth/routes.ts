55c89ab7a55878232617d5b00bb90f8ef70ae3ca
Patocv14 <PatricioCervantes140207@hotmail.com>
2024-03-30 15:37:31 -0600

    Se injectan las dependencias necesarias para el controller

--- src/presentation/auth/routes.ts
+++ src/presentation/auth/routes.ts
@@ -1,10 +1,14 @@
 import { Router } from 'express';
 import { AuthController } from './controller';
+import { AuthDatasourceImpl, AuthRepositoryImpl } from '../../infrestructure';
 
 export class AuthRoutes {
   static get routes(): Router {
     const router = Router();
-    const controller = new AuthController();
+
+    const datasource = new AuthDatasourceImpl();
+    const authRepository = new AuthRepositoryImpl(datasource);
+    const controller = new AuthController(authRepository);
 
     // definir todas mis rutas principales
 
