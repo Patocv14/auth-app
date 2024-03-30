9fa32d7ddcab5950f1caf7e8b8150783c6dd635a
Patocv14 <PatricioCervantes140207@hotmail.com>
2024-03-30 15:32:39 -0600

    Se crea auth repository y datasource

---
+++ src/domain/data-sources/auth.datasource.ts
@@ -0,0 +1,7 @@
+import { User } from '../entities/user.entity';
+import { RegisterUserDto } from '../dtos/auth/register-user.dto';
+
+export abstract class AuthDataSource {
+  // abstract login(loginUserDto: LoginUserDto): Promise<User>;
+  abstract register(registerUserDto: RegisterUserDto): Promise<User>;
+}
